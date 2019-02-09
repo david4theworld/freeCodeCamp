685bb51be2c90988a6e1cae359051646cdaa1bce
david4theworld <david4theworld@gmail.com>
2019-02-09 18:29:26 +0100

    Refresh t s# Please enter the commit message for your changes. Lines starting

--- assets/js/test.js
+++
@@ -1,6 +0,0 @@
-var test = document.createElement("script");
-test.src = "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
-document.getElementsByTagName("body")[0].appendChild(test);
-var cssTest = document.getElementsByClassName(".fcc_test_ui");
-console.log("Got there ", cssTest);
-//style.setProperty("top", "700px");
